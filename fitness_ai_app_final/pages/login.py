b7ae282c985d080a8555c68d8b449419d434e6ca
ayushkhade0-debug <ayushkhade0@gmail.com>
2026-02-22 14:53:14 +0600

    Fix import path for Streamlit Cloud

--- fitness_ai_app_final/pages/login.py
+++ fitness_ai_app_final/pages/login.py
@@ -2,6 +2,8 @@ import streamlit as st
 import sys, os
 sys.path.insert(0, os.path.dirname(os.path.dirname(__file__)))
 from api_client import register, login, check_backend
+```
+
 
 def show():
     st.markdown("""
